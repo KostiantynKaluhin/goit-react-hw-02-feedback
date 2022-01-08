ef71df802911b8ea6c1ce53759e0980a720c1f00
Kostianyn <odessavshok@gmail.com>
2022-01-08 20:19:33 +0200

    add Feedback

---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import s from "./FeedbackOptions.module.css";
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map((option) => (
+        <button
+          className={s.button}
+          type="button"
+          name={option}
+          data-value={option}
+          onClick={onLeaveFeedback}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.shape({
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+  }).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
